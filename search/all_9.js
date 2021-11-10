75fc6a30923f233e5645762a2cb147162d93da80
ejovo13 <ejovo13@users.noreply.github.com>
2021-11-10 23:49:49 +0000

    deploy: aba0c24e5bfd88e260ade99c29b29140252489b4

--- search/all_9.js
+++ search/all_9.js
@@ -3,5 +3,10 @@ var searchData=
   ['reverse_5fstring_0',['reverse_string',['../ejovo__string_8h.html#a2fff45352dc3daf913d02c8484f1eb11',1,'ejovo_string.h']]],
   ['reverse_5fstring_5fn_1',['reverse_string_n',['../ejovo__string_8h.html#a4beb0ccca089d842a5b92a64175f7551',1,'ejovo_string.h']]],
   ['rol64_2',['rol64',['../ejovo__rand_8h.html#ab60efacee6d1141acada9ed8ccc2ad82',1,'ejovo_rand.h']]],
-  ['rowiter_5fnew_3',['RowIter_new',['../ejovo__matrix_8h.html#ac6992799b618681f50795b939d73d284',1,'ejovo_matrix.h']]]
+  ['rowiter_5fapply_3',['RowIter_apply',['../ejovo__matrix_8h.html#adfc91d3b437f857a3d20b592696700ab',1,'ejovo_matrix.h']]],
+  ['rowiter_5fapply_5fset_5fiter_4',['RowIter_apply_set_iter',['../ejovo__matrix_8h.html#a0bf098ebb1213825ee35acdd14bdc8b9',1,'ejovo_matrix.h']]],
+  ['rowiter_5fapply_5fset_5fk_5',['RowIter_apply_set_k',['../ejovo__matrix_8h.html#a526d95fc526abbb366b87b80a90e99cf',1,'ejovo_matrix.h']]],
+  ['rowiter_5fapply_5fset_5fptr_6',['RowIter_apply_set_ptr',['../ejovo__matrix_8h.html#ae3832f94c0878928981236e4318115e1',1,'ejovo_matrix.h']]],
+  ['rowiter_5fnew_7',['RowIter_new',['../ejovo__matrix_8h.html#ac6992799b618681f50795b939d73d284',1,'ejovo_matrix.h']]],
+  ['rowiter_5frow_5fset_5fk_8',['RowIter_row_set_k',['../ejovo__matrix_8h.html#a51fccf4b6edbc37be803bca546ea1315',1,'ejovo_matrix.h']]]
 ];
